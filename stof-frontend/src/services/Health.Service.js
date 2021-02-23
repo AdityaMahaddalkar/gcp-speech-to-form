ddb504f3afe70867ecd0980d04b904c04e4b3f63
Aditya Mahaddalkar <adityam1311@gmail.com>
2021-02-23 15:06:34 +0530

    Added debugging code to service

--- stof-frontend/src/services/Health.Service.js
+++ stof-frontend/src/services/Health.Service.js
@@ -4,6 +4,7 @@ import { baseURLs } from "../utils/Base.URLs";
 const env = process.env.NODE_ENV || "local";
 export const getHealth = (setHealth) => {
   const endpoint = `${baseURLs[env]}/html/health`;
+  console.log(`${env}:${endpoint}`);
   Axios.get(endpoint).then((response) => {
     if (response.status === 200) {
       setHealth(response.data.health);
